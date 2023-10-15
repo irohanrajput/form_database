b5d18938bbfad9202def8bb41e8c2adedf4b13e5
irohanrajput <itusharsingh27@gmail.com>
2023-10-15 05:55:23 +0530

    learned clearly about storing data into the database

--- baseapp/views.py
+++ baseapp/views.py
@@ -3,10 +3,10 @@ from .models import addDetails
 
 def index_views(request):
     if request.method == 'POST':
-        fullname = request.POST['name']
+        fullname = request.POST['fullname']
         email = request.POST['email']
         mobile = request.POST['phone']
-        yviews = request.POST['ybiews']
+        yviews = request.POST['yviews']
 
 
         new_query = addDetails(Name=fullname, email=email, phone=mobile, yviews=yviews )
