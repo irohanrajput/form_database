22a92546c2b39ae39abc9212fe8d0a0282656ccc
irohanrajput <itusharsingh27@gmail.com>
2023-10-16 23:17:30 +0530

    added new fields in the form and synced with the database

--- baseapp/models.py
+++ baseapp/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
-class addDetails(models.Model):
+class Details(models.Model):
     Name = models.CharField(max_length=200, null=False, blank=False)
     email = models.CharField(max_length=200, null=False, blank=False)
-    phone = models.IntegerField(("Phone number"))
-    yviews = models.CharField(max_length=200, null=False, blank=False)
-
+    phone = models.CharField(max_length=20, null=False, blank=False )  
+    What_Users_Say = models.CharField(max_length=200, null=False, blank=False)
+    if_selected = models.CharField(max_length=200, null=False, blank=False)
+    # if_selected = models.BooleanField()
     def __str__(self):
         return self.title
--- baseapp/views.py
+++ baseapp/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
-from .models import addDetails
+from .models import Details
 
 def index_views(request):
     if request.method == 'POST':
         fullname = request.POST['fullname']
         email = request.POST['email']
         mobile = request.POST['phone']
-        yviews = request.POST['yviews']
+        checkbox1value = request.POST.get('checkbox1', 'False')
+        if checkbox1value == 'True':
+            sendvalue = "yes"
 
-
-        new_query = addDetails(Name=fullname, email=email, phone=mobile, yviews=yviews )
+        print(fullname)
+        print(sendvalue)
+        new_query = Details(Name=fullname, email=email, phone=mobile,    if_selected=sendvalue )
         new_query.save()
 
     return render (request, 'baseapp/index.html')
