b3d2143bfe1806f08189ee2c4aaac73f19bee1a1
irohanrajput <itusharsingh27@gmail.com>
2023-10-27 16:46:42 +0530

    still fixing the page to show detailsd

--- baseapp/models.py
+++ baseapp/models.py
@@ -4,8 +4,9 @@ class Details(models.Model):
     Name = models.CharField(max_length=200, null=False, blank=False)
     email = models.CharField(max_length=200, null=False, blank=False)
     phone = models.CharField(max_length=20, null=False, blank=False )  
-    What_Users_Say = models.CharField(max_length=200, null=False, blank=False)
-    if_selected = models.CharField(max_length=200, null=False, blank=False, default=False)
+    user_feedback = models.CharField(max_length=200, null=False, blank=False)
+    contact_via_phone = models.CharField(max_length=200, null=False, blank=False, default=False)
+    contact_via_email = models.CharField(max_length=200, null=False, blank=False, default=False)
 
     def __str__(self):
         return f'{self.Name}'
--- baseapp/views.py
+++ baseapp/views.py
@@ -7,8 +7,11 @@ def form_view(request):
         fullname = request.POST['fullname']
         email = request.POST['email']
         mobile = request.POST['phone']
+        your_views = request.POST['yviews']
         callcheckbox = request.POST.get('callme', 'False')
-        new_query = Details( Name=fullname, email=email, phone=mobile, if_selected=callcheckbox )
+        emailcheckbox = request.POST.get('emailme', 'False')
+        
+        new_query = Details( Name=fullname, email=email, phone=mobile, contact_via_phone=callcheckbox, contact_via_email=emailcheckbox, user_feedback = your_views )
         new_query.save()
 
     return render (request, 'baseapp/form.html')
